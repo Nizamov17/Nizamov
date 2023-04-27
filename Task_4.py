5020922a3d6f91960a021afe4de0a5b8acbf20db
DESKTOP-7RS0IBC\USER <13_artur.nizamov_13@mail.ru>
2023-04-27 16:59:46 +0300

    lesson_16 Task_1 Task_2 Task_3 Task_4 Task_5

---
+++ Task_4.py
@@ -0,0 +1,25 @@
+class User:
+    def __init__(self, login, password):
+        self.login = login
+        self.password = password
+
+    def view(self):
+        print("Я - User. Могу просматривать содержимое")
+
+class Moderator(User):
+    def __init__(self, login, password, group_id):
+        super().__init__(login, password)
+        self.group_id = group_id
+
+    def view(self):
+        print("Я - Moderator. Могу просматривать содержимое")
+
+    def redact(self):
+        print("Я - Moderator. Могу редактировать содержимое")
+
+user = User("super", 1234)
+user.view()
+
+moderator = Moderator("delovoy", 1234, 123)
+moderator.view()
+moderator.redact()
